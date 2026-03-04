58c6be879e63eced95533d253a0e2ea8a749ff17
nexforgecom <runi81730@gmail.com>
2026-03-04 16:05:26 +0700

    Update wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,10 +1,9 @@
-// lib/wagmi.ts
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { base } from 'wagmi/chains'; // Import Base chain dari wagmi
+import { base } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'NexxForge v2',
-  projectId: 'YOUR_WALLET_CONNECT_PROJECT_ID', // Dapat dari https://cloud.walletconnect.com
-  chains: [base], // Fokus Base only
-  ssr: true, // Support SSR di Next.js/Vercel
+  projectId: '1ca2482f7aa1c65ad3d2b2e143c89ef4',
+  chains: [base],
+  ssr: true,
 });
