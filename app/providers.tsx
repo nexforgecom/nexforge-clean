cdab63b30c9c51e32717402d70a56344f9223716
nexforgecom <runi81730@gmail.com>
2026-03-04 23:10:49 +0700

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '@/lib/apolloClient';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { customTheme } from '@/lib/theme';
 import { config } from '@/lib/wagmi';
 
@@ -10,12 +12,14 @@ const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={customTheme}>
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <ApolloProvider client={client}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider theme={customTheme}>
+            {children}
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ApolloProvider>
   );
 }
